2b21bf27211a0acce15c627fdd7972c2c53a30c8
Amit kumar <amitk.kumar414@gmail.com>
2025-05-27 23:52:56 +0530

    data deleted

--- assets/data/manifest.js
+++ assets/data/manifest.js
@@ -1,199 +1,199 @@
 export const courseFiles = {
-  nodejs: {
-    modules: [
-      {
-        filename: "module01.json",
-        module: require("../../assets/data/courses/nodejs/modules/module01.json"),
-      },
-      {
-        filename: "module02.json",
-        module: require("../../assets/data/courses/nodejs/modules/module02.json"),
-      },
-      {
-        filename: "module03.json",
-        module: require("../../assets/data/courses/nodejs/modules/module03.json"),
-      },
-      {
-        filename: "module04.json",
-        module: require("../../assets/data/courses/nodejs/modules/module04.json"),
-      },
-      {
-        filename: "module05.json",
-        module: require("../../assets/data/courses/nodejs/modules/module05.json"),
-      },
-      {
-        filename: "module06.json",
-        module: require("../../assets/data/courses/nodejs/modules/module06.json"),
-      },
-      {
-        filename: "module07.json",
-        module: require("../../assets/data/courses/nodejs/modules/module07.json"),
-      },
-      {
-        filename: "module08.json",
-        module: require("../../assets/data/courses/nodejs/modules/module08.json"),
-      },
-      {
-        filename: "module09.json",
-        module: require("../../assets/data/courses/nodejs/modules/module09.json"),
-      },
-      {
-        filename: "module10.json",
-        module: require("../../assets/data/courses/nodejs/modules/module10.json"),
-      },
-      ,
-      {
-        filename: "module11.json",
-        module: require("../../assets/data/courses/nodejs/modules/module11.json"),
-      },
-      {
-        filename: "module12.json",
-        module: require("../../assets/data/courses/nodejs/modules/module12.json"),
-      },
-      ,
-      {
-        filename: "module13.json",
-        module: require("../../assets/data/courses/nodejs/modules/module13.json"),
-      },
-      {
-        filename: "module14.json",
-        module: require("../../assets/data/courses/nodejs/modules/module14.json"),
-      },
-      {
-        filename: "module15.json",
-        module: require("../../assets/data/courses/nodejs/modules/module15.json"),
-      },
+  // nodejs: {
+  //   modules: [
+  //     {
+  //       filename: "module01.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module01.json"),
+  //     },
+  //     {
+  //       filename: "module02.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module02.json"),
+  //     },
+  //     {
+  //       filename: "module03.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module03.json"),
+  //     },
+  //     {
+  //       filename: "module04.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module04.json"),
+  //     },
+  //     {
+  //       filename: "module05.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module05.json"),
+  //     },
+  //     {
+  //       filename: "module06.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module06.json"),
+  //     },
+  //     {
+  //       filename: "module07.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module07.json"),
+  //     },
+  //     {
+  //       filename: "module08.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module08.json"),
+  //     },
+  //     {
+  //       filename: "module09.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module09.json"),
+  //     },
+  //     {
+  //       filename: "module10.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module10.json"),
+  //     },
+  //     ,
+  //     {
+  //       filename: "module11.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module11.json"),
+  //     },
+  //     {
+  //       filename: "module12.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module12.json"),
+  //     },
+  //     ,
+  //     {
+  //       filename: "module13.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module13.json"),
+  //     },
+  //     {
+  //       filename: "module14.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module14.json"),
+  //     },
+  //     {
+  //       filename: "module15.json",
+  //       module: require("../../assets/data/courses/nodejs/modules/module15.json"),
+  //     },
     
    
      
-    ],
-    flashcards: [
-      {
-        filename: "flashcard_module01.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module01.json"),
-      },
-      {
-        filename: "flashcard_module02.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module02.json"),
-      },
-      {
-        filename: "flashcard_module03.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module03.json"),
-      },
-      {
-        filename: "flashcard_module04.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module04.json"),
-      },
-      {
-        filename: "flashcard_module05.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module05.json"),
-      },
-      {
-        filename: "flashcard_module06.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module06.json"),
-      },
-      {
-        filename: "flashcard_module07.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module07.json"),
-      },
-      {
-        filename: "flashcard_module08.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module08.json"),
-      },
-      {
-        filename: "flashcard_module09.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module09.json"),
-      },
-      {
-        filename: "flashcard_module10.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module10.json"),
-      },
-      {
-        filename: "flashcard_module11.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module11.json"),
-      },
-      {
-        filename: "flashcard_module12.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module12.json"),
-      },
-      {
-        filename: "flashcard_module13.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module13.json"),
-      },
-      {
-        filename: "flashcard_module14.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module14.json"),
-      },
-      {
-        filename: "flashcard_module15.json",
-        module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module15.json"),
-      },
+  //   ],
+  //   flashcards: [
+  //     {
+  //       filename: "flashcard_module01.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module01.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module02.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module02.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module03.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module03.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module04.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module04.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module05.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module05.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module06.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module06.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module07.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module07.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module08.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module08.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module09.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module09.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module10.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module10.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module11.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module11.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module12.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module12.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module13.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module13.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module14.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module14.json"),
+  //     },
+  //     {
+  //       filename: "flashcard_module15.json",
+  //       module: require("../../assets/data/courses/nodejs/flashcards/flashcard_module15.json"),
+  //     },
     
   
-    ],
-    quizzes: [
-      {
-        filename: "quiz_module01.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module01.json"),
-      },
-      {
-        filename: "quiz_module02.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module02.json"),
-      },
-      {
-        filename: "quiz_module03.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module03.json"),
-      },
-      {
-        filename: "quiz_module04.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module04.json"),
-      },
-      {
-        filename: "quiz_module05.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module05.json"),
-      },
-      {
-        filename: "quiz_module06.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module06.json"),
-      },
-      {
-        filename: "quiz_module07.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module07.json"),
-      },
-      {
-        filename: "quiz_module08.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module08.json"),
-      },
-      {
-        filename: "quiz_module09.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module09.json"),
-      },
-      {
-        filename: "quiz_module10.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module10.json"),
-      },
-      {
-        filename: "quiz_module11.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module11.json"),
-      },
-      {
-        filename: "quiz_module12.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module12.json"),
-      },
-      {
-        filename: "quiz_module13.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module13.json"),
-      },
-      {
-        filename: "quiz_module14.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module14.json"),
-      },
-      {
-        filename: "quiz_module15.json",
-        module: require("../../assets/data/courses/nodejs/quizzes/quiz_module15.json"),
-      },
+  //   ],
+  //   quizzes: [
+  //     {
+  //       filename: "quiz_module01.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module01.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module02.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module02.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module03.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module03.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module04.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module04.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module05.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module05.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module06.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module06.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module07.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module07.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module08.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module08.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module09.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module09.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module10.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module10.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module11.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module11.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module12.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module12.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module13.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module13.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module14.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module14.json"),
+  //     },
+  //     {
+  //       filename: "quiz_module15.json",
+  //       module: require("../../assets/data/courses/nodejs/quizzes/quiz_module15.json"),
+  //     },
    
     
-    ],
-  },
+  //   ],
+  // },
 };
